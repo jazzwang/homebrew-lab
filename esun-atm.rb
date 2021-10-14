f50923bcf2bd24fcb77514b6a6c8a1afa225c8df
jazzwang <jazzwang.tw@gmail.com>
2021-10-14 10:11:30 +0800

    add esun-atm

---
+++ esun-atm.rb
@@ -0,0 +1,6 @@
+cask "esun-atm" do
+  desc "ActiveX plugin for ESun WebATM"
+  homepage "https://easynetbank.esunbank.com.tw/#/download"
+  url "https://easynetbank.esunbank.com.tw/assets/ActiveX/ESUNATM_ServiceInstall.pkg"
+  version "1.0.0.5"
+end
