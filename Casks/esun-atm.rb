22fa60c540b1eb7c243923f4cf93a02b2d19c3e3
jazzwang <jazzwang.tw@gmail.com>
2021-10-16 10:58:12 +0800

    update esum-atm

--- Casks/esun-atm.rb
+++ Casks/esun-atm.rb
@@ -1,6 +1,8 @@
 cask "esun-atm" do
+  version "1.0.0.5"
+  name "ESun WebATM ActiveX Plugin"
   desc "ActiveX plugin for ESun WebATM"
   homepage "https://easynetbank.esunbank.com.tw/#/download"
   url "https://easynetbank.esunbank.com.tw/assets/ActiveX/ESUNATM_ServiceInstall.pkg"
-  version "1.0.0.5"
+  pkg "ESUNATM_ServiceInstall.pkg"
 end
