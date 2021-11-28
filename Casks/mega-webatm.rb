8c02b05beb633719333a61957408d21b79c3a319
jazzwang <jazzwang.tw@gmail.com>
2021-11-28 21:30:38 +0800

    update mega-webatm.rb

--- Casks/mega-webatm.rb
+++ Casks/mega-webatm.rb
@@ -4,6 +4,7 @@ cask "mega-webatm" do
       verified: "netbank.megabank.com.tw/natm/resource/common"
   name "MegaBank WebATM ActiveX Plugin"
   desc "ActiveX plugin for MegaBank WebATM"
+  sha256 "6b11582cbca5a1f2d2d7296b510e78dddd0130790dd8d6479fbd55b10871eb30"
   homepage "https://netbank.megabank.com.tw/natm/"
   pkg "MegaWebATM.app"
 end
